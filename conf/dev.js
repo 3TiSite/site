1bc36b8e6d241e4c5f187497d3dca3468a73c55c
3Ti <3Ti.Site@gmail.com>
2023-12-11 11:41:11 +0800

    ♨

--- conf/dev.js
+++ conf/dev.js
@@ -1,3 +1,3 @@
-export const MD = "//md.xxai.art/";
-export const API = "//3ti.site/";
+export const MD = "//127.0.0.1:7776/";
+export const API = "//127.0.0.1:7775/";
 export const I18N = "/i18n/";
