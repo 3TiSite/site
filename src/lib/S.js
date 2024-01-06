0cb24cdfe78503ec4607a4dd081ffc34f52814e8
3Ti <3Ti.Site@gmail.com>
2024-01-06 14:16:38 +0800

    ♨

--- src/lib/S.js
+++ src/lib/S.js
@@ -1,52 +1,52 @@
-import { captcha } from "@~3/captcha";
-import lang from "@~3/lang";
-import { toastErr } from "@~3/toast";
-import fBin from "@3-/fetch/fBinPrefix.js";
+import { captcha } from "@~3/captcha"
+import lang from "@~3/lang"
+import { toastErr } from "@~3/toast"
+import fBin from "@3-/fetch/fBinPrefix.js"
 
-import { API } from "~/conf.js";
-const fbin = fBin(API);
+import { API } from "~/conf.js"
+const fbin = fBin(API)
 
-var AUTH;
+let AUTH
 
-export const setAuth = (f) => (AUTH = f);
+export const setAuth = (f) => (AUTH = f)
 
 export const req = async (url, opt) => {
-	opt.method = opt.method || "POST";
-	opt.headers = opt.headers || {};
-	opt.headers["Accept-Language"] = lang();
-	opt.credentials = "include";
+	opt.method = opt.method || "POST"
+	opt.headers = opt.headers || {}
+	opt.headers["Accept-Language"] = lang()
+	opt.credentials = "include"
 	try {
-		return await fbin(url, opt);
+		return await fbin(url, opt)
 	} catch (r) {
-		var { status } = r;
+		var { status } = r
 		if (status) {
 			switch (status) {
 				case 417: // form error
-          try {
-            r = await r.json();
-          } catch (err) {
-            r = err;
-          } else {
-            throw r
-          }
+					try {
+						r = await r.json()
+					} catch (err) {
+						r = err
+						break
+					}
+					throw r
 				case 401:
 					return new Promise((resolve, reject) => {
-						AUTH(() => req(url, opt).then(resolve, reject));
-					});
+						AUTH(() => req(url, opt).then(resolve, reject))
+					})
 				case 412: // captcha error
-					return captcha(url, opt.body, new Uint8Array(await r.arrayBuffer()));
+					return captcha(url, opt.body, new Uint8Array(await r.arrayBuffer()))
 			}
 		}
 		setTimeout(async () => {
 			try {
-				r = await r.text();
+				r = await r.text()
 			} finally {
-				toastErr(r);
+				toastErr(r)
 			}
-		});
-		throw r;
+		})
+		throw r
 	}
-};
+}
 
 export default new Proxy(
 	{},
@@ -54,11 +54,11 @@ export default new Proxy(
 		get:
 			(_, url) =>
 			async (...args) => {
-				var opt = {};
+				var opt = {}
 				if (args.length) {
-					opt.body = JSON.stringify(args.length == 1 ? args[0] : args);
+					opt.body = JSON.stringify(args.length == 1 ? args[0] : args)
 				}
-				return req(url, opt);
+				return req(url, opt)
 			},
 	},
-);
+)
