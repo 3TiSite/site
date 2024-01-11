68da4571991dd3a4028fdcc6e18408b1cde28dc9
3Ti <3Ti.Site@gmail.com>
2024-01-11 14:16:07 +0800

    ♨

--- conf/dev.js
+++ conf/dev.js
@@ -1,3 +1,3 @@
-export const MD = "//localhost:7776/"
-export const API = "//localhost:7775/"
+export const MD = "//127.0.0.1:7776/"
+export const API = "//127.0.0.1:7775/"
 export const I18N = "/i18n/"
