3e7177fd5219674b0c76234411f45c8ea1762a4c
3Ti <3Ti.Site@gmail.com>
2024-01-12 17:01:20 +0800

    ♨

--- sh/dev.sh
+++ sh/dev.sh
@@ -6,7 +6,9 @@ cd $DIR/..
 set -ex
 kill -9 $(lsof -i:$VITE_PORT -t) 2>/dev/null | true
 
-bash -c "sleep 1 && open https://127.0.0.1:$VITE_PORT" &
+if ! command -v open &>/dev/null; then
+  bash -c "sleep 1 && open https://127.0.0.1:$VITE_PORT" &
+fi
 
 bun x concurrently --kill-others \
   -r "bun x vite"
